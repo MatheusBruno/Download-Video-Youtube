3987c5698957810f6e2013a843a0670b64f78672
MatheusBruno <matheusbrunosilveira@gmail.com>
2023-09-03 16:08:24 -0300

    .editado

--- downloadvideo.py
+++ downloadvideo.py
@@ -1,5 +1,12 @@
 from pytube import YouTube
-link = input("Download Link: ")
-url = input("Caminho: ")
+import sys
+
+if len(sys.argv) > 1:
+    link = sys.argv[1]
+    url = sys.argv[2]
+else:
+    link = input("Download Link: ")
+    url = input("Caminho: ")
+
 url = str(url).replace('\\','/')
 YouTube(link).streams.get_highest_resolution().download(url)
